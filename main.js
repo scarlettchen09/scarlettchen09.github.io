4b62f50fce3f8d710da646a4a5173fddb5f1b0a1
scarlettchen09 <39448078+scarlettchen09@users.noreply.github.com>
2021-06-03 04:19:05 -0700

    updated code

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-const notesPerCol = 8;
-const numOfCols = 8;
 
+// import * as Tone from "./node_modules/tone/build/Tone.js"
+import * as SynthSettings from "../synth-settings.js"
+
+const notesPerCol = 10;
+const numOfCols = 16;
+
+let scale = getScale('A3', notesPerCol, "pentatonic");
+
+let grid = makeGrid(scale);
+let musicNotes = convertGridToMusicNotes();
+
+let buttonGrid = document.createElement("div");
+document.body.appendChild(buttonGrid);
+buttonGrid.className = "button-grid";
 function getScale(firstNote, numNotes, scaleType) {
     let scale = {
         major: [0, 2, 4, 5, 7, 9, 11, 12],
@@ -17,7 +29,7 @@ function getScale(firstNote, numNotes, scaleType) {
     return freq.slice(0, numNotes);
 }
 
-const makeGrid = function (colNotes) {
+function makeGrid (colNotes) {
     colNotes = colNotes.reverse();
     let grid = [];
     for (let i = 0; i < numOfCols; i++) {
@@ -28,15 +40,6 @@ const makeGrid = function (colNotes) {
         grid.push(col);
     }
     return grid;
-}
-
-function updateGrid(grid, colNotes) {
-    colNotes = colNotes.reverse();
-    grid.forEach((col) => {
-        col.forEach((n, nIndex) => {
-            n.note = colNotes[nIndex];
-        })
-    })
 }
 
 function handleButtonClick(e, colIndex, noteIndex) {
@@ -50,9 +53,8 @@ function handleButtonClick(e, colIndex, noteIndex) {
     }
     musicNotes = convertGridToMusicNotes();
     console.log(e.currentTarget.className + " " + e.currentTarget.id);
-
-
 }
+
 function makeButtons () {
     grid.forEach((col, colIndex) => {
         let column = document.createElement("div");
@@ -89,18 +91,68 @@ function convertGridToMusicNotes() {
     return playedNotes;
 }
 
+function updateGrid(colNotes) {
+    colNotes = colNotes.reverse();
+    grid.forEach((col) => {
+        col.forEach((n, nIndex) => {
+            n.note = colNotes[nIndex];
+        })
+    })
+}
 
-let scale = getScale('A3', notesPerCol, "pentatonic");
-var grid = makeGrid( scale);
-var musicNotes = convertGridToMusicNotes();
-var buttonGrid = document.createElement("div");
-document.body.appendChild(buttonGrid);
-buttonGrid.className = "button-grid";
+function makeScaleSettingsDiv() {
+    let scaleSettings = document.createElement("div");
+    scaleSettings.innerHTML = "<b>scale settings</b>";
+    scaleSettings.className = "scale-info";
+    document.body.appendChild(scaleSettings);
+
+    return scaleSettings;
+}
+
+function handleScaleSelection(e) {
+    console.log(e.target.value + notesPerCol);
+    let scale = getScale('A3', notesPerCol, e.target.value);
+    updateGrid(scale);
+    musicNotes = convertGridToMusicNotes();
+}
+
+function createScaleSelector() {
+    let scaleSelector = document.createElement("select");
+    scaleSelector.size = 3;
+    scaleSelector.addEventListener('change', (e) => {
+        handleScaleSelection(e);
+    })
+    scaleSettingsDiv.appendChild(scaleSelector);
+    let scaleTypes = ["major", "minor", "pentatonic"];
+    for (let i = 0; i < scaleTypes.length; i++) {
+        let scaleOption = document.createElement("option");
+        scaleOption.value = scaleTypes[i];
+        scaleOption.text = scaleTypes[i];
+        scaleSelector.appendChild(scaleOption);
+    }
+    return scaleSelector;
+}
 
 
-const synth = new Tone.PolySynth().toDestination();
-
-makeButtons();
+async function startPlaying () {
+    await Tone.start();
+    if (!isPlaying) {
+        playButton.innerHTML = "stop";
+        isPlaying = true;
+        await Sequencer.start();
+        await Tone.Transport.start();
+    } else {
+        isPlaying = false;
+        playButton.innerHTML = "play";
+        await Tone.Transport.stop();
+        await Sequencer.stop();
+        for (let i = 0; i < numOfCols; i++) {
+            buttonGrid.childNodes[i].className = buttonGrid.childNodes[i].className.replace("cur-col", "");
+        }
+        //await Sequencer.clear();
+        //await Sequencer.dispose();
+    }
+}
 
 function generateSequencerEvents() {
     let seq = [];
@@ -127,180 +179,46 @@ const Sequencer = new Tone.Sequence(
     "8n"
 );
 
-//Sequencer.start();
-let isPlaying = false;
-let playbtn = document.createElement("button");
-playbtn.className = "play"
-playbtn.innerHTML = "play";
-document.body.appendChild(playbtn);
-playbtn.addEventListener("click", startPlaying);
-
-async function startPlaying () {
-    await Tone.start();
-    if (!isPlaying) {
-        playbtn.innerHTML = "stop";
-        isPlaying = true;
-        await Sequencer.start();
-        await Tone.Transport.start();
-    } else {
-        isPlaying = false;
-        playbtn.innerHTML = "play";
-        await Tone.Transport.stop();
-        await Sequencer.stop();
-        for (let i = 0; i < numOfCols; i++) {
-            buttonGrid.childNodes[i].className = buttonGrid.childNodes[i].className.replace("cur-col", "");
-        }
-        //await Sequencer.clear();
-        //await Sequencer.dispose();
-    }
+function makePlayButton(){
+    let isPlaying = false;
+    let playbtn = document.createElement("button");
+    playbtn.className = "play"
+    playbtn.innerHTML = "play";
+    document.body.appendChild(playbtn);
+    playbtn.addEventListener("click", startPlaying);
+    return [isPlaying, playbtn];
 }
 
-const musicInfo = document.createElement("div");
-musicInfo.innerHTML = "<b> current scale: </b>";
-document.body.appendChild(musicInfo);
+function makeBPMSlider() {
+    let slider = document.createElement("input");
+    let bpmLabel = document.createElement("label");
+    bpmLabel.innerHTML = "BPM: 120";
+    slider.type = "range";
+    slider.min = "60";
+    slider.max = "240";
+    slider.step= "1";
+    slider.value = "120"
+    slider.addEventListener('change', (e) => {
+        Tone.Transport.bpm.value = e.target.value;
+        bpmLabel.innerHTML = "BPM:" + e.target.value;
+    })
 
-function handleScaleSelection(e) {
-    console.log(e.target.value + notesPerCol);
-    scale = getScale('A3', notesPerCol, e.target.value);
-    updateGrid(grid, scale);
-    musicNotes = convertGridToMusicNotes();
+    scaleSettingsDiv.appendChild(slider);
+    scaleSettingsDiv.insertBefore(bpmLabel, slider);
+    return slider;
+
 }
 
-function createScaleSelector() {
-    let scaleSelector = document.createElement("select");
-    scaleSelector.size = 3;
-    scaleSelector.addEventListener('change', (e) => {
-        handleScaleSelection(e);
-    })
-    musicInfo.appendChild(scaleSelector);
-    scaleTypes = ["major", "minor", "pentatonic"];
-    for (let i = 0; i < scaleTypes.length; i++) {
-        let scaleOption = document.createElement("option");
-        scaleOption.value = scaleTypes[i];
-        scaleOption.text = scaleTypes[i];
-        scaleSelector.appendChild(scaleOption);
-    }
-    return scaleSelector;
-}
-const synthOptions = document.createElement("div");
-synthOptions.innerHTML = "<b>synth settings:</b>";
-synthOptions.className = "synth-info";
-document.body.appendChild(synthOptions);
-const crusher = new Tone.BitCrusher(4).toDestination();
-const omniOsc = new Tone.OmniOscillator("C#4", "pwm").toDestination();
-synth.set ({
-    envelope: {
-        attack: 0,
-        decay: .5,
-        sustain: .5,
-        release: .3
-    }
-});
-function makeADSRSliders() {
-    let ADSRdiv = document.createElement("div");
-    ADSRdiv.className = "adsr";
-    synthOptions.appendChild(ADSRdiv);
-    let attackLabel = document.createElement("label");
-    attackLabel.innerHTML = "attack";
-
-    let decayLabel = document.createElement("label");
-    decayLabel.innerHTML = "decay";
-
-    let sustainLabel = document.createElement("label");
-    sustainLabel.innerHTML  = "sustain";
-
-    let releaseLabel = document.createElement("label");
-    releaseLabel.innerHTML = "release";
-
-    let attackSlider = document.createElement("input");
-    attackSlider.type = "range";
-    attackSlider.min = "0";
-    attackSlider.max = "8";
-    attackSlider.step= "0.1";
-    attackSlider.value = "0";
-    ADSRdiv.appendChild(attackSlider);
-    attackSlider.addEventListener('change', (e) => {
-        console.log(e.target.value);
-        synth.set ({
-            envelope: {
-                attack: e.target.value
-            }
-        });
-    })
-
-    let decaySlider = document.createElement("input");
-    decaySlider.type = "range";
-    decaySlider.min = "0";
-    decaySlider.max = "8";
-    decaySlider.step= "0.1";
-    decaySlider.value = ".5";
-    ADSRdiv.appendChild(decaySlider);
-    decaySlider.addEventListener('change', (e) => {
-        console.log(e.target.value);
-        synth.set ({
-            envelope: {
-                decay: e.target.value
-            }
-        });
-    })
-
-    let sustainSlider = document.createElement("input");
-    sustainSlider.type = "range";
-    sustainSlider.min = "0";
-    sustainSlider.max = "1";
-    sustainSlider.step= "0.03";
-    sustainSlider.value = ".5"
-    ADSRdiv.appendChild(sustainSlider);
-    sustainSlider.addEventListener('change', (e) => {
-        console.log(e.target.value);
-        synth.set ({
-            envelope: {
-                sustain: e.target.value
-            }
-        });
-    })
-
-    let releaseSlider = document.createElement("input");
-    releaseSlider.type = "range";
-    releaseSlider.min = ".1";
-    releaseSlider.max = "8";
-    releaseSlider.step= "0.1";
-    releaseSlider.value = ".3"
-    ADSRdiv.appendChild(releaseSlider);
-    releaseSlider.addEventListener('change', (e) => {
-        console.log(e.target.value);
-        synth.set ({
-            envelope: {
-                release: e.target.value
-            }
-        });
-    })
-    ADSRdiv.insertBefore(attackLabel, attackSlider);
-    ADSRdiv.insertBefore(decayLabel, decaySlider);
-    ADSRdiv.insertBefore(sustainLabel, sustainSlider);
-    ADSRdiv.insertBefore(releaseLabel, releaseSlider);
-}
-
-function makeEffectOptions() {
-    let crushCheck = document.createElement("input");
-    let crushLabel = document.createElement("crushLabel");
-    crushLabel.innerHTML = "bitcrush";
-
-    crushCheck.type = "checkbox";
-    crushCheck.addEventListener('change', (e) => {
-        if (e.target.checked) {
-            synth.connect(crusher);
-        } else {
-            synth.disconnect(crusher);
-        }
-    })
-
-    synthOptions.appendChild(crushCheck);
-    synthOptions.insertBefore(crushLabel, crushCheck);
-
-}
+let scaleSettingsDiv = makeScaleSettingsDiv();
 let scaleSelector = createScaleSelector();
-let slider = makeADSRSliders();
-let effectOptions = makeEffectOptions();
+
+const [synth, synthOptions] = SynthSettings.makeOptionsDiv();
+
+makeButtons();
+
+let [isPlaying, playButton] = makePlayButton();
+
+let bpmSlider = makeBPMSlider();
+const omniOsc = new Tone.OmniOscillator("C#4", "pwm").toDestination();
 
 scaleSelector.selectedIndex = 2;
---
+++ synth-settings.js
@@ -0,0 +1,141 @@
+
+function makeOptionsDiv() {
+    let synth = new Tone.PolySynth(Tone.AMSynth).toDestination();
+    //synth.disconnect();
+    // synth.dispose();
+    // synth = null;
+    // synth = new Tone.PolySynth(Tone.PluckSynth).toDestination();
+    synth.set( {
+        voice: Tone.MetalSynth
+
+    });
+
+    synth.set ({
+        envelope: {
+            attack: 0,
+            decay: .5,
+            sustain: .5,
+            release: .3
+        }
+    });
+
+    let synthOptions = document.createElement("div");
+    synthOptions.innerHTML = "<b>synth settings:</b>";
+    synthOptions.className = "synth-info";
+    document.body.appendChild(synthOptions);
+
+
+    makeADSRSliders(synthOptions, synth);
+    makeEffectOptions(synthOptions, synth);
+    return [synth, synthOptions];
+}
+
+function makeADSRSliders(synthOptions, synth) {
+    let ADSRdiv = document.createElement("div");
+    ADSRdiv.className = "adsr";
+    synthOptions.appendChild(ADSRdiv);
+    let attackLabel = document.createElement("label");
+    attackLabel.innerHTML = "attack: ";
+
+    let decayLabel = document.createElement("label");
+    decayLabel.innerHTML = "decay: ";
+
+    let sustainLabel = document.createElement("label");
+    sustainLabel.innerHTML  = "sustain: ";
+
+    let releaseLabel = document.createElement("label");
+    releaseLabel.innerHTML = "release: ";
+
+    let attackSlider = document.createElement("input");
+    attackSlider.type = "range";
+    attackSlider.min = "0";
+    attackSlider.max = "5";
+    attackSlider.step= "0.1";
+    attackSlider.value = "0";
+    ADSRdiv.appendChild(attackSlider);
+    attackLabel.innerHTML += attackSlider.value;
+    attackSlider.addEventListener('change', (e) => {
+        attackLabel.innerHTML = "attack: " + e.target.value;
+        synth.set ({
+            envelope: {
+                attack: e.target.value
+            }
+        });
+    })
+
+    let decaySlider = document.createElement("input");
+    decaySlider.type = "range";
+    decaySlider.min = "0.5";
+    decaySlider.max = "4";
+    decaySlider.step= "0.1";
+    decaySlider.value = ".5";
+    ADSRdiv.appendChild(decaySlider);
+    decayLabel.innerHTML += decaySlider.value;
+    decaySlider.addEventListener('change', (e) => {
+        decayLabel.innerHTML = "decay: " + e.target.value;
+        synth.set ({
+            envelope: {
+                decay: e.target.value
+            }
+        });
+    })
+
+    let sustainSlider = document.createElement("input");
+    sustainSlider.type = "range";
+    sustainSlider.min = "0";
+    sustainSlider.max = "1";
+    sustainSlider.step= "0.03";
+    sustainSlider.value = ".5"
+    ADSRdiv.appendChild(sustainSlider);
+    sustainLabel.innerHTML += sustainSlider.value;
+    sustainSlider.addEventListener('change', (e) => {
+        sustainLabel.innerHTML = "sustain: " + e.target.value;
+        synth.set ({
+            envelope: {
+                sustain: e.target.value
+            }
+        });
+    })
+
+    let releaseSlider = document.createElement("input");
+    releaseSlider.type = "range";
+    releaseSlider.min = ".1";
+    releaseSlider.max = "5";
+    releaseSlider.step= "0.1";
+    releaseSlider.value = ".3"
+    ADSRdiv.appendChild(releaseSlider);
+    releaseLabel.innerHTML += releaseSlider.value;
+    releaseSlider.addEventListener('change', (e) => {
+        releaseLabel.innerHTML = "release: " + e.target.value;
+        synth.set ({
+            envelope: {
+                release: e.target.value
+            }
+        });
+    })
+    ADSRdiv.insertBefore(attackLabel, attackSlider);
+    ADSRdiv.insertBefore(decayLabel, decaySlider);
+    ADSRdiv.insertBefore(sustainLabel, sustainSlider);
+    ADSRdiv.insertBefore(releaseLabel, releaseSlider);
+}
+
+function makeEffectOptions(synthOptions, synth) {
+    let pingPong = new Tone.PingPongDelay("4n", 0.2).toDestination();
+    let pingPongCheck = document.createElement("input");
+    let pingPongLabel = document.createElement("label");
+    pingPongLabel.innerHTML = "ping pong delay";
+
+    pingPongCheck.type = "checkbox";
+    pingPongCheck.addEventListener('change', (e) => {
+        if (e.target.checked) {
+            synth.connect(pingPong);
+        } else {
+            synth.disconnect(pingPong);
+        }
+    })
+
+    synthOptions.appendChild(pingPongCheck);
+    synthOptions.insertBefore(pingPongLabel, pingPongCheck);
+
+}
+export {makeOptionsDiv};
